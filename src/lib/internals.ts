aa086f1368736affd1e8314b66e6885f3f8ac617
Levent Arman Özak <arman@armanozak.com>
2019-02-18 04:57:40 +0300

    Import and export useful NGXS internals

---
+++ src/lib/internals.ts
@@ -0,0 +1,5 @@
+import { MetaDataModel, StateClass } from '@ngxs/store/src/internal/internals';
+import { getValue, setValue } from '@ngxs/store/src/utils/utils';
+import { NgxsNextPluginFn } from '@ngxs/store/src/symbols';
+
+export { getValue, MetaDataModel, NgxsNextPluginFn, setValue, StateClass };
