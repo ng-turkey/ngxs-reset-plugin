9c054bb3df8dc9654173789017a1a36ae0e01c2f
Levent Arman Özak <arman@armanozak.com>
2019-02-18 05:31:24 +0300

    Add specs to suite and improve plugin to pass them

--- src/lib/reset.plugin.ts
+++ src/lib/reset.plugin.ts
@@ -1,10 +1,87 @@
 import { Injectable } from '@angular/core';
-import { NgxsPlugin } from '@ngxs/store';
-import { NgxsNextPluginFn } from './internals';
+import { getActionTypeFromInstance, NgxsPlugin } from '@ngxs/store';
+import { getValue, MetaDataModel, setValue } from './internals';
+import { StateClear, StateReset, StateOverwrite, getMetaData } from './symbols';
 
 @Injectable()
 export class NgxsResetPlugin implements NgxsPlugin {
-  handle(state: any, action: any, next: NgxsNextPluginFn) {
+  private clearStates(state: any, statesToKeep: MetaDataModel[]): any {
+    return statesToKeep
+      .map(meta => getPath(meta))
+      .map(path => ({
+        parts: path.split('.'),
+        value: getValue(state, path),
+      }))
+      .reduce(
+        (obj, { parts, value }) =>
+          parts.reduceRight(
+            (acc, part) =>
+              part in obj
+                ? {
+                    [part]: {
+                      ...obj[part],
+                      ...acc,
+                    },
+                  }
+                : { [part]: acc },
+            value,
+          ),
+        {} as any,
+      );
+  }
+
+  private overwriteStates(
+    state: any,
+    statesToOverwrite: MetaDataModel[],
+    values: any[],
+  ): any {
+    statesToOverwrite.forEach(
+      (meta, index) => (state = setValue(state, getPath(meta), values[index])),
+    );
+    return state;
+  }
+
+  private resetStates(state: any, statesToReset: MetaDataModel[]): any {
+    statesToReset.forEach(meta => {
+      state = setValue(state, getPath(meta), meta.defaults);
+
+      if (meta.children) {
+        state = this.resetStates(state, meta.children.map(
+          getMetaData,
+        ) as MetaDataModel[]);
+      }
+    });
+
+    return state;
+  }
+
+  handle(state: any, action: any, next: any) {
+    const type: string = getActionTypeFromInstance(action) || '';
+
+    switch (type) {
+      case StateClear.type:
+        const { statesToKeep } = <StateClear>action;
+        state = this.clearStates(state, statesToKeep);
+        break;
+
+      case StateReset.type:
+        const { statesToReset } = <StateReset>action;
+        state = this.resetStates(state, statesToReset);
+        break;
+
+      case StateOverwrite.type:
+        const { statesToOverwrite, values } = <StateOverwrite>action;
+        state = this.overwriteStates(state, statesToOverwrite, values);
+        break;
+
+      default:
+        break;
+    }
+
     return next(state, action);
   }
+}
+
+function getPath(meta: MetaDataModel): string {
+  return meta.path || '';
 }
--- src/tests/reset.plugin.spec.ts
+++ src/tests/reset.plugin.spec.ts
@@ -1,8 +1,13 @@
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { Actions, NgxsModule, Store } from '@ngxs/store';
-import { NgxsResetPluginModule } from '../public_api';
+import {
+  NgxsResetPluginModule,
+  StateClear,
+  StateOverwrite,
+  StateReset,
+} from '../public_api';
 import { AppState, PreferencesState, SessionState, ToDoState } from './test-states';
-import { ToDoAdd } from './test-symbols';
+import { SessionEnd, ToDoAdd } from './test-symbols';
 
 interface TestModel {
   actions$: Actions;
@@ -10,10 +15,131 @@ interface TestModel {
 }
 
 describe('NgxsResetPlugin', () => {
-  it('should be ready for testing', fakeAsync(() => {
+  it('should clear states on StateClear', fakeAsync(() => {
     const { store } = setupTest();
+
+    store.dispatch(new StateClear());
+    tick();
+
+    expect(store.snapshot()).toEqual({});
+  }));
+
+  it('should clear states on StateClear but keep selected', fakeAsync(() => {
+    const { store } = setupTest();
+
+    store.dispatch(new StateClear(PreferencesState));
+    tick();
+
+    expect(store.snapshot()).toEqual({
+      app: { preferences: { darkmode: false, language: 'en' } },
+    });
+  }));
+
+  it('should clear states on StateClear but keep selected (multi)', fakeAsync(() => {
+    const { store } = setupTest();
+
+    const lastseen = ensureLastSeen(store);
+
+    store.dispatch(new StateClear(PreferencesState, SessionState));
+    tick();
+
+    expect(store.snapshot()).toEqual({
+      app: { preferences: { darkmode: false, language: 'en' }, session: { lastseen } },
+    });
+  }));
+
+  it('should log a warning on StateClear with wrong payload', fakeAsync(() => {
+    const { store } = setupTest();
+    const state = store.snapshot();
+
+    console.warn = jasmine.createSpy('warning');
+
+    store.dispatch(new StateClear(ToDoAdd));
+    tick();
+
+    expect(console.warn).toHaveBeenCalled();
+    expect(store.snapshot()).toEqual({});
+  }));
+
+  it('should reset state to defaults on StateReset', fakeAsync(() => {
+    const { store } = setupTest();
+
+    store.dispatch(new StateReset(AppState)); // should respect state tree
+    tick();
+
+    expect(store.selectSnapshot(ToDoState.list)).toEqual([]);
+  }));
+
+  it('should reset state to defaults on StateReset (multi)', fakeAsync(() => {
+    const { store } = setupTest();
+
+    ensureLastSeen(store);
+
+    store.dispatch(new StateReset(SessionState, ToDoState));
+    tick();
+
+    expect(store.selectSnapshot(SessionState)).toBeUndefined();
+    expect(store.selectSnapshot(ToDoState.list)).toEqual([]);
+  }));
+
+  it('should log a warning on StateReset with wrong payload', fakeAsync(() => {
+    const { store } = setupTest();
+    const state = store.snapshot();
+
+    console.warn = jasmine.createSpy('warning');
+
+    store.dispatch(new StateReset(ToDoAdd));
+    tick();
+
+    expect(console.warn).toHaveBeenCalled();
+    expect(store.snapshot()).toEqual(state);
+  }));
+
+  it('should overwrite state with given value on StateOverwrite', fakeAsync(() => {
+    const { store } = setupTest();
+
+    store.dispatch(new StateOverwrite([ToDoState, { list: [] }]));
+    tick();
+
+    expect(store.selectSnapshot(ToDoState.list)).toEqual([]);
+  }));
+
+  it('should overwrite state with given value on StateOverwrite (multi)', fakeAsync(() => {
+    const { store } = setupTest();
+
+    ensureLastSeen(store);
+
+    store.dispatch(new StateOverwrite([SessionState, null], [ToDoState, { list: [] }]));
+    tick();
+
+    expect(store.selectSnapshot(SessionState)).toBeNull();
+    expect(store.selectSnapshot(ToDoState.list)).toEqual([]);
+  }));
+
+  it('should log a warning on StateOverwrite with wrong payload', fakeAsync(() => {
+    const { store } = setupTest();
+    const state = store.snapshot();
+
+    console.warn = jasmine.createSpy('warning');
+
+    store.dispatch(new StateOverwrite([ToDoAdd, true]));
+    tick();
+
+    expect(console.warn).toHaveBeenCalled();
+    expect(store.snapshot()).toEqual(state);
   }));
 });
+
+function ensureLastSeen(store: Store): number {
+  const lastseen = new Date().valueOf();
+
+  store.dispatch(new SessionEnd(lastseen));
+  tick();
+
+  expect(store.selectSnapshot(SessionState)).toEqual({ lastseen });
+
+  return lastseen;
+}
 
 function setupTest(): TestModel {
   TestBed.configureTestingModule({
