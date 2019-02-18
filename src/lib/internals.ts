896777009f5e9723f47562b3c92b832d69c9dfa9
Levent Arman Özak <arman@armanozak.com>
2019-02-18 05:24:57 +0300

    Replace internal imports with actual code from NGXS codebase

--- src/lib/internals.ts
+++ src/lib/internals.ts
@@ -1,5 +1,40 @@
-import { MetaDataModel, StateClass } from '@ngxs/store/src/internal/internals';
-import { getValue, setValue } from '@ngxs/store/src/utils/utils';
-import { NgxsNextPluginFn } from '@ngxs/store/src/symbols';
+import { MetaDataModel } from '@ngxs/store/src/internal/internals';
 
-export { getValue, MetaDataModel, NgxsNextPluginFn, setValue, StateClass };
+export { MetaDataModel };
+
+/**
+ * from @ngxs/store/src/utils/utils
+ */
+export const setValue = (obj: any, prop: string, val: any) => {
+  obj = { ...obj };
+
+  const split = prop.split('.');
+  const lastIndex = split.length - 1;
+
+  split.reduce((acc, part, index) => {
+    if (index === lastIndex) {
+      acc[part] = val;
+    } else {
+      acc[part] = Array.isArray(acc[part]) ? [...acc[part]] : { ...acc[part] };
+    }
+
+    return acc && acc[part];
+  }, obj);
+
+  return obj;
+};
+
+/**
+ * from @ngxs/store/src/utils/utils
+ */
+export const getValue = (obj: any, prop: string): any =>
+  prop.split('.').reduce((acc: any, part: string) => acc && acc[part], obj);
+
+/**
+ * a simplified implementation of NGXS StateClass interface
+ */
+export interface StateClass<T = {}> {
+  NGXS_META?: MetaDataModel;
+
+  new (...args: any[]): T;
+}
